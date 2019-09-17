68d080712784820cfb60a937c46c4da2738a0418
Adityesh <adityeshmishra98@gmail.com>
2019-09-17 19:41:09 +0530

    Modified Script

---
+++ sendEmail.py
@@ -0,0 +1,17 @@
+import smtplib
+
+s = smtplib.SMTP('smtp.gmail.com',587)
+
+s.starttls()
+
+email_input = input("Enter your email id: \n")
+email_password = input("Enter your email password: \n")
+
+s.login(email_input,email_password)
+
+message = input("Enter the message you want to send: \n")
+receiver = input("Enter the email of the person you want to send the message: \n")
+
+s.sendmail(email_input,receiver,message)
+
+s.quit()
